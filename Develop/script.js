ab2e4f5d88497667ccb8ce353b7be3df6f4e6236
Mike Veneziale <MikeVeneziale@Mikes-MBP.attlocal.net>
2021-07-22 21:25:16 -0500

    aligning date boxes

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,6 @@
 // Displays current date
 var displayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(displayDate);
+
+// Time block status, displays grey red or green
+$(".time-block")
