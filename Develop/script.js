9e1f6199f7d96e0aec559ed0bf5b9cdfb3d18c23
Mike Veneziale <falciformdesign@gmail.com>
2021-07-24 14:20:21 -0500

    change colors of textarea based on time of day

--- Develop/script.js
+++ Develop/script.js
@@ -2,5 +2,29 @@
 var displayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(displayDate);
 
+var hourCheck = moment().hour();
+console.log(hourCheck);
+
+  
+
+
 // Time block status, displays grey red or green
-$(".time-block")
+ if (hourCheck > 12) {
+     $("textarea").addClass("past");
+} else if (hourCheck === 9) {
+    $("textarea").addClass("present");
+} else {
+    $("textarea").addClass("future");
+}
+
+// on click save button saving to local storage the variable of textEntry
+
+document.querySelector(".saveBtn").addEventListener("click", function() {
+    var textEntry = document.querySelector(".description").value;
+    console.log(textEntry);
+    localStorage.setItem("event", textEntry);
+
+})
+
+document.querySelector(".description").value = localStorage.getItem("event");
+
