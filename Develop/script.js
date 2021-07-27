2c80a3587c77a39e0b9d56986b891f19a0540123
Mike Veneziale <falciformdesign@gmail.com>
2021-07-26 21:10:35 -0500

    added color coding to hour blocks of schedule depending on time of day

--- Develop/script.js
+++ Develop/script.js
@@ -3,29 +3,52 @@ var displayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(displayDate);
 
 var hourCheck = moment().hour();
-console.log(hourCheck);
-
-  
 
 
-// Time block status, displays grey red or green
- if (hourCheck > 12) {
-     $("textarea").addClass("past");
-} else if (hourCheck === 9) {
-    $("textarea").addClass("present");
-} else {
-    $("textarea").addClass("future");
+// const currentHour = 13;
+
+// const shadeDiv = function(h) {
+//     const classDynamic = `.${h}`
+//     const timeNow = document.querySelector(classDynamic)
+//     $(`.${h}`).addClass("present");
+//     console.log(classDynamic);
+//     console.log(timeNow);
+
+// }
+
+
+
+//   for loop 
+
+
+// Time block status, timeValue variable targets the ID plus a number 9-18. If the current time in hourCheck is greater than
+// the i value, add a particular class to timeValue, which equals the target of # plus i
+for (i = 9; i < 18; i++) {
+    let timeValue = $("#" + i);
+    // var hourCheck = 11;
+    var hourCheck = moment().hour();
+
+  if (hourCheck > i) {
+     timeValue.addClass("past");
+ } else if (hourCheck === i) {
+     timeValue.addClass("present");
+ } else {
+     timeValue.addClass("future");
+    //  $("textarea").addClass("future");
+ }
+
 }
+// end of for loop
+// });
+
 
 // on click save button saving to local storage the variable of textEntry
 
 document.querySelector(".saveBtn").addEventListener("click", function() {
     var textEntry = document.querySelector(".description").value;
-    console.log(textEntry);
     localStorage.setItem("event", textEntry);
 
 })
 
 document.querySelector(".description").value = localStorage.getItem("event");
 
-// make a for loop
